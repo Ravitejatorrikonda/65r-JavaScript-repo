f4c0db7cb39c0422cb84eedd342a46bfab8cb172
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2025-11-26 12:51:47 +0530

    JSON

---
+++ Day-19/index.js
@@ -0,0 +1,49 @@
+/**
+ * 
+ * https://dummyjson.com/quotes
+ * 
+ * https://dummyjson.com/todos
+ * 
+ * https://jsonplaceholder.typicode.com/photos
+ * 
+ * https://pokeapi.co/api/v2/pokemon/ditto
+ * 
+ * 
+ */
+// 
+
+// const URL = "https://pokeapi.co/api/v2/pokemon/ditto"
+// fetch(URL).then(res => res.json())
+//     .then(data => console.log(data.game_indices))
+//     .catch(data1 => console.log(data1))
+
+// const URL = "https://jsonplaceholder.typicode.com/photos";
+// fetch(URL).then(res => res.json())
+//     .then(data => console.log(data))
+
+
+// fetch(URL, options)
+
+/**
+ * Method:-
+ * 
+ * GET, POST,PUT,PATCH, DELETE
+ * 
+ * 
+ */
+// let options={
+//     "method" : "GET",
+// }
+
+// fetch("https://dummyjson.com/quotes",{"methods": "GET"})
+//     .then(response => response.json()).then(data=>console.log(data))
+
+/**
+ * http://localhost:3000/studentData
+ */
+// fetch("http://localhost:3000/studentData/", { "method": "GET" })
+//     .then(response => response.json()).then(data=>console.log(data)
+// )
+
+// fetch("http://localhost:3000/Data").then(res=>res.json())
+// .then(data=>console.log(data)).catch(data=>console.warn("Data Not Found"))
