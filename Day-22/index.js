dce7027defd6a34ac6e939dbf32f99f94e21f1cf
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2025-12-02 12:47:30 +0530

    Array methods

---
+++ Day-22/index.js
@@ -0,0 +1,63 @@
+/**
+ * 
+ * fetch methods:-
+ * ---------------
+ * GET POST PUT PATCH DELETE
+ * 
+ * 
+ * Array methods:-
+ * --------------
+ * collection of single type of data
+ * 
+ * var identifier=[,,,,,]
+ * 
+ */
+//        0        1     2     3       4 5 6 7
+// var arr=["HTML", "CSS", "JS", "React",1,2,3,4]
+// arr[0]="Boostrap"
+// console.log(arr);
+
+/**
+ * 
+ * diff types of methodds:-
+ * --------------------------
+ * 
+ * at
+ * concat
+ * splice
+ * pop
+ * push
+ * shift 
+ * unshift
+ * slice
+ * 
+ * 
+ * 
+ */
+//              
+// let arr=[1,2,3,4,5]
+// arr.splice(2,2,4)
+// console.log(arr);
+
+
+// let array=["HTML", "CSS","js","react","node"]
+// let newarray=array.slice(0,3)
+// console.log(newarray);// css js
+
+// let arr=[1,2,3,4,"html"]
+// let newarr=arr.pop()
+// console.log(newarr);
+// console.log(arr.pop());
+//         40  50 60  40 50 60
+// let arr=[10,20,30,40,50,60]
+// arr.copyWithin(0,3)
+
+     let arr=["a", "b", "c" ,"d","e",["f", ["g", ["h","i"]]]]; 
+    
+        console.log(arr.flat()) 
+
+
+
+
+
+
